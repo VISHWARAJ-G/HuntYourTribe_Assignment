1f7f098a63b449519e138bb3e7cca50239581f51
VISHWARAJ-G <rajgvishwa630@gmail.com>
2025-04-06 11:06:39 +0530

    Displayed Logo Name

--- Huntyourtribe_Frontend/src/components/Notification.jsx
+++ Huntyourtribe_Frontend/src/components/Notification.jsx
@@ -1,19 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Notification() {
+  const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch("http://localhost:5000/notifications");
-        const data = await response.json();
-        console.log(data);
+        const jsondata = await response.json();
+        console.log(jsondata);
+        setData(jsondata);
       } catch (error) {
         console.error("Error while fetching:", error);
       }
     }
     fetchData();
   }, []);
-  return <></>;
+  return (
+    <>
+      <div className="pt-24 px-10">
+        {data.map((value) => {
+          return (<div className="flex justify-between">
+            <div className="text-7xl">{value["icon"]}</div>
+            <div>{value["message"]}</div>
+          </div>);
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Notification;
